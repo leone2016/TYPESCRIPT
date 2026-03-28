150f997d0012c9e232e95ebbd5a7f76b4a238db9
Leonardo Medina <leoz.31@hotmail.com>
2026-03-28 18:10:49 -0500

    feat(class1): inherit

---
+++ clase1/src/05-herencia.ts
@@ -0,0 +1,44 @@
+console.log("¡Clase 1!");
+class MeseroBasico{
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+
+    }
+    servirCafeAmericano(){
+        console.log("servir cafe con leche");
+    }
+
+    servirCafeConLeche(){
+        console.log("servir cafe con leche");
+    }
+}
+
+class MeseroModerno extends MeseroBasico{
+    lastName: string;
+    constructor(name: string, lastName: string) {
+        super(name);
+        this.lastName = lastName;
+
+    }
+    servirCapuccino(){
+        console.log("Servir Capuccino");
+    }
+
+    print(){
+        console.log(` name: ${this.name}, lastName: ${this.lastName}`);
+    }
+}
+
+console.log("============ Mesero Basico ============")
+const meseroBasico = new MeseroBasico("Carlo Antonio");
+meseroBasico.servirCafeAmericano();
+meseroBasico.servirCafeConLeche();
+console.log(" :: "+ meseroBasico.name)
+console.log("\n\n============ Mesero Moderno ============")
+
+const meseroModerno = new MeseroModerno("Leonard", "Medina");
+meseroModerno.servirCafeAmericano();
+meseroModerno.servirCafeConLeche();
+meseroModerno.servirCapuccino();
+meseroModerno.print()
